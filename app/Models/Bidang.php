326e09b28419ccc57a92c0d503a6a56a1e1b306e
nannnmuzaki <162042041+nannnmuzaki@users.noreply.github.com>
2025-07-09 11:42:10 +0700

    feat: add sisaKuota() on Bidang model

--- app/Models/Bidang.php
+++ app/Models/Bidang.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class Bidang extends Model
 {
@@ -33,6 +34,25 @@ class Bidang extends Model
     protected $casts = [
         'kuota' => 'integer',
     ];
+
+    /**
+     * 
+     * Membuat atribut virtual 'sisa_kuota'.
+     */
+    protected function sisaKuota(): Attribute
+    {
+        return Attribute::make(
+            get: function ($value, $attributes) {
+                // Periksa apakah 'pengajuan_diterima_count' sudah di-load oleh withCount untuk mencegah error.
+                if (!isset($attributes['pengajuan_diterima_berlangsung_count'])) {
+                    return 'N/A';
+                }
+
+                // Hitung sisa kuota: kuota total - jumlah pengajuan yang diterima/berlangsung.
+                return $attributes['kuota'] - $attributes['pengajuan_diterima_berlangsung_count'];
+            }
+        );
+    }
 
     public function pengajuan(): HasMany
     {
